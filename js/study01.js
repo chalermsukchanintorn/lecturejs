59c3a047893b91848e600291d9c02374ae6008c2
chalermsukchanintorn <chalermsukchanintorn@gmail.com>
2025-02-25 12:31:18 +0700

    JS Beginner Complete

--- js/study01.js
+++ js/study01.js
@@ -1,18 +1,17 @@
-console.log('ABC...')
-console.log("ABC...")
-console.log(123456) 
+//data type in javascript
+
+//String
+console.log("ABC..."); console.log('ABC')
+//Number
+console.log(123456) ;
 console.log(115.546) 
+//Boolean
 console.log(true)  //false
-
-//ตัวแปร
-var a = 10 //เป็นแบบ Global ใช้ที่ไหนก็ได้
-let b = 20  //เป็นแบบ Local ใช้ได้เฉพาะใน { } หนึ่งๆ เท่านั้น ***
-const c = 30 //ไม่สามารถเปลี่ยนค่าได้ เป็นแบบ Local ใช้ได้เฉพาะใน { } หนึ่งๆ เท่านั้น
-
-let data01 = 'Wow....'
-let data02 = 123456
-
-//เวลาเอาข้อมูลหลากหลายแบบมาใช้ร่วมกัน
-console.log('AAA ' + 555 + '  ' + data01 + '     ' + data02 )
-
-console.log(`AAA ${555}  ${data01}     ${data02}`)
+//Array
+console.log([1, 2, 3, 4, 5, 'Hello...', true])
+//Object
+console.log({fullname:'John', age:20})
+//Null
+console.log(null);
+//Undefined
+console.log(undefined)
